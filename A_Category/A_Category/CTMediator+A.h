27dbdb21744162cb7c6d3f74af6cf94153bd00e0
钱盼盼 <qpanpan@qiujieda.com>
2018-11-27 17:35:59 +0800

    1

---
+++ A_Category/A_Category/CTMediator+A.h
@@ -0,0 +1,17 @@
+//
+//  CTMediator+A.h
+//  A_Category
+//
+//  Created by qianpanpan on 2018/11/23.
+//  Copyright © 2018 qianpanpan. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+
+
+
+@interface CTMediator (A)
+- (UIViewController *)A_aViewController;
+@end
+
+
